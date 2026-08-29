eb97678ea8c94e883d2e833adcfe62476473b3b9
Rony Santra <santrarony9@gmail.com>
2026-08-29 15:21:26 +0530

    fix ts error and deploy db fix

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 let MONGODB_URI = process.env.MONGODB_URI || "";
 if (MONGODB_URI && !MONGODB_URI.includes("dsrevent_db")) {
-  MONGODB_URI = MONGODB_URI.replace("mongodb.net/?", "mongodb.net/dsrevent_db?");
+  const parts = MONGODB_URI.split("mongodb.net");
+  if (parts.length === 2) {
+    MONGODB_URI = parts[0] + "mongodb.net/dsrevent_db" + (parts[1].startsWith("/") ? parts[1].substring(1) : parts[1]);
+  }
 }
 let cached = (global as any).mongoose;
 
